e3079dae5fbddc382dbf2ad03c0b81ce39477761
bookslomot <alexgol2101@gmail.com>
2022-04-19 00:14:42 +0400

    fix

--- school_task/urls.py
+++ school_task/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework_swagger.views import get_swagger_view
@@ -31,4 +33,8 @@ urlpatterns = [
     re_path('api/', include('djoser.urls.authtoken')),
     path('api/', include('djoser.urls.jwt')),
 
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
